292d993b7a6d32e1418f5e62cb9dde07c916486a
victoria Andrade Rocha <victoriaandrade71@gmail.com>
2022-06-18 05:31:23 -0300

    Iniciando o GIT

---
+++ public/js/index.js
@@ -0,0 +1,7 @@
+//CRIAR CONTA
+document.getElementById("create-form").addEventListener("submit", function(e){
+    e.preventDefault();
+    
+    const email=document.getElementById("email-create-input"), value;
+    const senha=document.getElementById("password-create-input"), value;
+})
